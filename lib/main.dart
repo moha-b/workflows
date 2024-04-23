30fb0d7c8f78451288d0baf7c290e73c2b875f08
hab <mohab.khalid.mahmoud@gmail.com>
2024-04-23 14:07:28 +0200

    try to test formatting

--- lib/main.dart
+++ lib/main.dart
@@ -40,6 +40,10 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    //
+    //
+    //
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
